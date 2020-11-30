b08b2538414ced0d0d073982098fe3dd6814bfa4
dfoxpro <dfoxpro@gmail.com>
2020-11-30 11:23:07 -0500

    new constant data

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,10 @@
 const API_DOMAIN = 'https://pokeapi.co/api/v2/'
+const API_HEADER = { 'x-pokey': 'Agumon' }
+const MAX_API_RETRY = 10
 
 const COLOUR_A = 'aquamarine'
 const COLOUR_B = 'pink'
 const COLOUR_C = 'beige'
 const COLOUR_FOREGROUND = 'black'
 
-export { API_DOMAIN, COLOUR_A, COLOUR_B, COLOUR_C, COLOUR_FOREGROUND }
+export { API_DOMAIN, API_HEADER, MAX_API_RETRY, COLOUR_A, COLOUR_B, COLOUR_C, COLOUR_FOREGROUND }
